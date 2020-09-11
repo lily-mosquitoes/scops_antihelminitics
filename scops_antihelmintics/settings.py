0957c865fc30783db6ffe600a8cb5d808425a521
Lily <lily.mosquitoes@gmail.com>
2020-09-11 11:36:05 -0300

    changed database from mysql to postgresql

--- scops_antihelmintics/settings.py
+++ scops_antihelmintics/settings.py
@@ -74,7 +74,7 @@ url = urlparse(os.environ['DATABASE_URL'])
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': url.path[1:],
         'USER': url.username,
         'PASSWORD': url.password,
